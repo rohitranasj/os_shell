54b8c0fdda246ee793e0b38c9314d52ab49e1668
rohitranasj <63275190+rohitranasj@users.noreply.github.com>
2020-04-07 22:23:21 +0530

    Update osshell.c

--- osshell.c
+++ osshell.c
@@ -11,3 +11,29 @@
 static char* args[512];
 pid_t pid;
 int command_pipe[2];
+void getInput()
+{
+    fflush(stdout); // clear all previous buffers if any
+    input = NULL;
+    ssize_t buf = 0;
+    getline(&input,&buf,stdin);
+    // Copy into another string if we need to run special executables
+    input1 = (char *)malloc(strlen(input) * sizeof(char));
+    strncpy(input1,input,strlen(input));
+    argcount = 0;inBackground = 0;
+    while((argval[argcount] = strsep(&input, " \t\n")) != NULL && argcount < ARGMAX-1)
+    {
+        // do not consider "" as a parameter
+        if(sizeof(argval[argcount])==0)
+        {
+            free(argval[argcount]);
+        }
+        else argcount++;
+        if(strcmp(argval[argcount-1],"&")==0)
+        {
+            inBackground = 1; //run in inBackground
+            return;
+        }
+    }
+    free(input);
+}
