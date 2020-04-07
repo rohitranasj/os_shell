12ef073b08f5dc2cb8ed5a52e7a4289ad35aa0e3
rohitranasj <63275190+rohitranasj@users.noreply.github.com>
2020-04-07 08:51:14 +0530

    Create osshell.c

---
+++ osshell.c
@@ -0,0 +1,13 @@
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
+#include <unistd.h>
+ 
+#include <sys/types.h>
+#include <sys/wait.h>
+ 
+static char* args[512];
+pid_t pid;
+int command_pipe[2];
