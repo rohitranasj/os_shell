44bc81e69c270c0b4c917185447c5e1d2c599644
rohitranasj <63275190+rohitranasj@users.noreply.github.com>
2020-04-08 09:40:16 +0530

    Update osshell.c

--- osshell.c
+++ osshell.c
@@ -1,16 +1,60 @@
-
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <ctype.h>
-#include <unistd.h>
- 
 #include <sys/types.h>
 #include <sys/wait.h>
- 
-static char* args[512];
-pid_t pid;
-int command_pipe[2];
+#include<stdlib.h>
+#include<stdio.h>
+#include <unistd.h>
+#include <string.h>
+#include <errno.h>
+#include <dirent.h>
+#include <sys/stat.h>
+#include <grp.h>
+#include <pwd.h>a
+#include <time.h>
+#include <fcntl.h>
+#define BUFSIZE /BUFS 1000
+#define INPBUF /INB 100
+#define ARGMAX ARGM 10
+#define GREEN /G "\x1b[92m"
+#define BLUE  /B "\x1b[94m"
+#define DEF  /D "\x1B[0m"
+#define CYAN  /C "\x1b[96m"
+
+
+
+
+char *input,*input1;
+//char input[1000];
+int exitflag = 0;
+int filepid,fd[2];
+char cwd[BUFS];
+char* argval[ARGM]; // our local argc, argv
+int argcount = 0,inBackground = 0;
+int externalIn=0,externalOut=0;
+char inputfile[INB],outputfile[INB];
+
+struct _instr
+{
+    char * argval[INB];
+    int argcount;
+};
+typedef struct _instr instruction;
+
+
+void stopSignal()
+{
+
+    if(filepid!=0)
+    {
+        int temp = filepid;
+        kill(filepid, SIGINT);
+        filepid = 0;
+
+    }
+}
+
+
+
+
 void getInput()
 {
     fflush(stdout); // clear all previous buffers if any
@@ -21,7 +65,7 @@ void getInput()
     input1 = (char *)malloc(strlen(input) * sizeof(char));
     strncpy(input1,input,strlen(input));
     argcount = 0;inBackground = 0;
-    while((argval[argcount] = strsep(&input, " \t\n")) != NULL && argcount < ARGMAX-1)
+    while((argval[argcount] = strsep(&input, " \t\n")) != NULL && argcount < ARGM-1)
     {
         // do not consider "" as a parameter
         if(sizeof(argval[argcount])==0)
@@ -36,4 +80,428 @@ void getInput()
         }
     }
     free(input);
+}
+
+
+
+
+
+
+void function_clear()
+{
+    const char* blank = "\e[1;1H\e[2J";
+    write(STDOUT_FILENO,blank,12);
+}
+
+/* remove folder */
+void function_rmdir(char* name)
+{
+    int statrm = rmdir(name);
+    if(statrm==-1)
+    {
+        perror("+--- Error in rmdir ");
+    }
+}
+
+
+/* Make folder */
+void function_mkdir(char* name)
+{
+    int stat = mkdir(name, 0777);// all appropriate permissions
+    if(stat==-1)
+    {
+        perror("+--- Error in mkdir ");
+    }
+}
+
+
+/*change directory functionality*/
+void function_cd(char* path)
+{
+    int ret = chdir(path);
+    if(ret==0) // path could be changed if cd successful
+    {
+        function_pwd(cwd,0);
+    }
+    else perror("+--- Error in cd ");
+}
+
+
+/*Implement basic exit*/
+int function_exit()
+{
+    exitflag = 1;
+    return 0; // return 0 to parent process in run.c
+}
+
+/* Implement pwd function in shell - 1 prints, 0 stores*/
+void function_pwd(char* cwdstr,int command)
+{
+    char temp[BUFS];
+    char* path=getcwd(temp, sizeof(temp));
+    if(path != NULL)
+    {
+        strcpy(cwdstr,temp);
+        if(command==1)  // check if pwd is to be printed
+        {
+            printf("%s\n",cwdstr);
+        }
+    }
+    else perror("+--- Error in getcwd() : ");
+
+}
+
+
+
+
+
+
+
+/* copy one file to another */
+void function_cp(char* file1, char* file2)
+{
+    FILE *f1,*f2;
+    struct stat t1,t2;
+    f1 = fopen(file1,"r");
+    if(f1 == NULL)
+    {
+        perror("+--- Error in cp file1 ");
+        return;
+    }
+    f2 = fopen(file2,"r");// if file exists
+    if(f2)
+    {
+        // file2 exists
+        // file1 must be more recently updated
+        stat(file1, &t1);
+        stat(file2, &t2);
+        if(difftime(t1.st_mtime,t2.st_mtime) < 0)
+        {
+            printf("+--- Error in cp : %s is more recently updated than %s\n",file2,file1);
+            fclose(f1);
+            fclose(f2);
+            return;
+        }
+    }
+    f2 = fopen(file2,"ab+"); // create the file if it doesn't exist
+    fclose(f2);
+
+    f2 = fopen(file2,"w+");
+    if(f2 == NULL)
+    {
+        perror("Error in cp file2 ");
+        fclose(f1);
+        return;
+    }
+    //if(access(file2,W_OK)!=0 || access(file1,R_OK)!=0 || access(file2,F_OK)!=0)
+    if(open(file2,O_WRONLY)<0 || open(file1,O_RDONLY)<0)
+    {
+        perror("Error in cp access ");
+        return;
+    }
+    char cp;
+    while((cp=getc(f1))!=EOF)
+    {
+        putc(cp,f2);
+    }
+
+    fclose(f1);
+    fclose(f2);
+}
+
+
+/* Just a fancy name printing function*/
+void nameFile(struct dirent* name,char* followup)
+{
+    if(name->d_type == DT_REG)          // regular file
+    {
+        printf("%s%s%s",B, name->d_name, followup);
+    }
+    else if(name->d_type == DT_DIR)    // a directory
+    {
+        printf("%s%s/%s",G, name->d_name, followup);
+    }
+    else                              // unknown file types
+    {
+        printf("%s%s%s",C, name->d_name, followup);
+    }
+}
+
+
+/*ls -l  lists date permissions etc*/
+void function_lsl()
+{
+    int i=0,total=0;
+    char timer[14];
+    struct dirent **listr;
+    struct stat details;
+    int listn = scandir(".",&listr,0,alphasort);
+    if(listn > 0)
+    {
+        printf("%s+--- Total %d objects in this directory\n",C,listn-2);
+        for ( i = 0; i < listn; i++)
+        {
+            if(strcmp(listr[i]->d_name,".")==0 || strcmp(listr[i]->d_name,"..")==0)
+            {
+                continue;
+            }
+            else if(stat(listr[i]->d_name,&details)==0)
+            {
+                // example - -rwxrwxr-x 1 user user  8872 Jan 26 10:19 a.out*
+                // owner permissions - group permissions - other permissions
+                // links associated - owner name - group name
+                // file size (bytes) - time modified - name
+                total += details.st_blocks; // block size
+                // owner permissions - group permissions - other permissions
+                printf("%s%1s",D,(S_ISDIR(details.st_mode)) ? "d" : "-");
+                printf("%s%1s",D,(details.st_mode & S_IRUSR) ? "r" : "-");
+                printf("%s%1s",D,(details.st_mode & S_IWUSR) ? "w" : "-");
+                printf("%s%1s",D,(details.st_mode & S_IXUSR) ? "x" : "-");
+                printf("%s%1s",D,(details.st_mode & S_IRGRP) ? "r" : "-");
+                printf("%s%1s",D,(details.st_mode & S_IWGRP) ? "w" : "-");
+                printf("%s%1s",D,(details.st_mode & S_IXGRP) ? "x" : "-");
+                printf("%s%1s",D,(details.st_mode & S_IROTH) ? "r" : "-");
+                printf("%s%1s",D,(details.st_mode & S_IWOTH) ? "w" : "-");
+                printf("%s%1s ",D,(details.st_mode & S_IXOTH) ? "x" : "-");
+                // links associated - owner name - group name
+                printf("%2ld ",(unsigned long)(details.st_nlink));
+                printf("%s ",(getpwuid(details.st_uid))->pw_name);
+                printf("%s ",(getgrgid(details.st_gid))->gr_name);
+                // file size (bytes) - time modified - name
+                printf("%5lld ",(unsigned long long)details.st_size);
+                strftime (timer,14,"%h %d %H:%M",localtime(&details.st_mtime));
+                printf("%s ",timer);
+                nameFile(listr[i],"\n");
+            }
+        }
+        printf("%s+--- Total %d object contents\n",CYAN,total/2);
+    }
+    else
+    {
+            printf("+--- Empty directory\n" );
+    }
+}
+
+/* list cwd contents*/
+void function_ls()
+{
+    int i=0;
+    struct dirent **listr;
+    int listn = scandir(".", &listr, 0, alphasort);
+    if (listn >= 0)
+    {
+        printf("%s+--- Total %d objects in this directory\n",C,listn-2);
+        for(i = 0; i < listn; i++ )
+        {
+            if(strcmp(listr[i]->d_name,".")==0 || strcmp(listr[i]->d_name,"..")==0)
+            {
+                continue;
+            }
+            else nameFile(listr[i],"    ");
+            if(i%8==0) printf("\n");
+        }
+        printf("\n");
+    }
+    else
+    {
+        perror ("+--- Error in ls ");
+    }
+
+}
+  
+  
+  
+  
+
+
+
+
+void runprocess(char * cli, char* args[], int count)
+{
+
+    int ret = execvp(cli, args);
+    char* pathm;
+    pathm = getenv("PATH");// check for all paths , executing non./
+    char path[1000];
+    strcpy(path, pathm);
+    strcat(path,":");
+    strcat(path,cwd);
+    char * cmd = strtok(path, ":\r\n");
+    while(cmd!=NULL)
+    {
+       char loc_sort[1000];
+        strcpy(loc_sort, cmd);
+        strcat(loc_sort, "/");
+        strcat(loc_sort, cli);
+        printf("execvp : %s\n",loc_sort );
+        ret = execvp(loc_sort, args);
+        if(ret==-1)
+        {
+            perror("+--- Error in running executable ");
+            exit(0);
+        }
+        cmd = strtok(NULL, ":\r\n");
+    }
+}
+
+
+
+
+/* executables like ./a.out */
+void executable()
+{
+    instruction command[INPBUF];
+    int i=0,j=1,status;
+    char* curr = strsep(&input1," \t\n");// need to do all over again
+                                // since we need to identify distinct commands
+    command[0].argval[0] = curr;
+
+    while(curr!=NULL)
+    {
+        curr = strsep(&input1, " \t\n");
+        if(curr==NULL)
+        {
+            command[i].argval[j++] = curr;
+        }
+        else if(strcmp(curr,"|")==0)
+        {
+            command[i].argval[j++] = NULL;
+            command[i].argcount = j;
+            j = 0;i++;// move to the next instruction
+        }
+        else if(strcmp(curr,"<")==0)
+        {
+            externalIn = 1;
+            curr = strsep(&input1, " \t\n");
+            strcpy(inputfile, curr);
+        }
+        else if(strcmp(curr,">")==0)
+        {
+            externalOut = 1;
+            curr = strsep(&input1, " \t\n");
+            strcpy(outputfile, curr);
+        }
+        else if(strcmp(curr, "&")==0)
+        {
+            inBackground = 1;
+        }
+        else
+        {
+            command[i].argval[j++] = curr;
+        }
+    }
+    command[i].argval[j++] = NULL; // handle last command separately
+    command[i].argcount = j;
+    i++;
+
+    // parent process waits for execution and then reads from terminl
+    filepid = fork();
+    if(filepid == 0)
+    {
+        pipe_dup(i, command);
+    }
+    else
+    {
+        if(inBackground==0)
+        {
+            waitpid(filepid,&status, 0);
+        }
+        else
+        {
+            printf("+--- Process running in inBackground. PID:%d\n",filepid);
+        }
+    }
+    filepid = 0;
+    free(input1);
+}  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+int main(int argc, char* argv[])
+{
+    signal(SIGINT,stopSignal);
+    int i;
+    int pipe1 = pipe(fd);
+    function_clear();
+    screenfetch();
+    function_pwd(cwd,0);
+
+    while(exitflag==0)
+    {
+        externalIn = 0; externalOut = 0;inBackground = 0;
+        printf("%s%s > ",D,cwd ); //print user prompt
+        // scanf("%s",input); - fails due to spaces, tabs
+        getInput();
+
+        if(strcmp(argval[0],"exit")==0 || strcmp(argval[0],"z")==0)
+        {
+            function_exit();
+        }
+      
+       
+        else if(strcmp(argval[0],"pwd")==0 && !inBackground)
+        {
+            function_pwd(cwd,1);
+        }
+        else if(strcmp(argval[0],"cd")==0 && !inBackground)
+        {
+            char* path = argval[1];
+            function_cd(path);
+        }
+        else if(strcmp(argval[0],"mkdir")==0 && !inBackground)
+        {
+            char* foldername = argval[1];
+            function_mkdir(foldername);
+        }
+        else if(strcmp(argval[0],"rmdir")==0 && !inBackground)
+        {
+            char* foldername = argval[1];
+            function_rmdir(foldername);
+        }
+        else if(strcmp(argval[0],"clear")==0 && !inBackground)
+        {
+            function_clear();
+        }
+        else if(strcmp(argval[0],"ls")==0 && !inBackground)
+        {
+            char* optional = argval[1];
+            if(strcmp(optional,"-l")==0 && !inBackground)
+            {
+                function_lsl();
+            }
+            else function_ls();
+        }
+        else if(strcmp(argval[0],"cp")==0 && !inBackground)
+        {
+            char* file1 = argval[1];
+            char* file2 = argval[2];
+            if(argcount > 2 && strlen(file1) > 0 && strlen(file2) > 0)
+            {
+                function_cp(file1,file2);
+            }
+            else
+            {
+                printf("+--- Error in cp : insufficient parameters\n");
+            }
+        }
+        else
+        {
+            executable();
+        }
+
+    }
+
 }
